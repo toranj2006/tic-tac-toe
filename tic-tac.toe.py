bcfce81e46fef381cf3068ffb44a0fb4162cd171
user <user@users-MacBook-Pro.local>
2024-12-17 23:15:04 -0500

    start

---
+++ tic-tac.toe.py
@@ -0,0 +1,10 @@
+import random
+
+print("Welcome to Tic Tac Toe")
+Print("----------------------")
+
+possibleNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+gameBoard = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+rows =
+cols =
