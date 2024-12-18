cf664b8b05176753936f482f4466a8e8d69b65ae
user <user@users-MacBook-Pro.local>
2024-12-17 23:30:17 -0500

    added incomplete function

--- tic-tac.toe.py
+++ tic-tac.toe.py
@@ -6,5 +6,10 @@ Print("----------------------")
 possibleNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 gameBoard = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-rows =
-cols =
+rows = 3
+cols = 3
+
+def printGameBoard():
+	for x in range(rows):
+		print("+---+---+---+")
+		
