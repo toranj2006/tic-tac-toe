538b0a682d09102eb534d8d6d69dcb1512d94a5f
user <user@users-MacBook-Pro.local>
2024-12-24 15:35:47 -0500

    finised turns

--- tic-tac.toe.py
+++ tic-tac.toe.py
@@ -17,4 +17,51 @@ def printGameBoard():
 			print("", gameBoard[x][y], end= " |")
 	print("\n+---+---+---+")
 
-printGameBoard()
+def modifyArray(num, turn):
+	num -= 1
+	if (num == 0):
+		gameBoard[0][0] = turn
+	elif (num == 1):
+		gameBoard[0][1] = turn
+	elif (num == 2):
+		gameBoard[0][2] = turn
+	elif (num == 3):
+		gameBoard[1][0] = turn
+	elif (num == 4):
+		gameBoard[1][1] = turn
+	elif (num == 5):
+		gameBoard[1][2] = turn
+	elif (num == 6):
+		gameBoard[2][0] = turn
+	elif (num == 7):
+		gameBoard[2][1] = turn
+	elif (num == 8):
+		gameBoard[2][2] = turn
+
+def checkForWinner
+
+leaveLoop = False
+turnCounter = 0
+
+while(leaveLoop == False):
+	#the player turn
+	if (turnCounter % 2 == 1):
+		printGameBoard()
+		numberPicked = int(input("\nChoose a number [1-9]: "))
+		if (numberPicked >= 1 or numberPicked <= 9):
+			modifyArray(numberPicked, 'X')
+			possibleNumbers.remove(numberPicked)
+		else:
+			print("Invalid input. Please try again!")
+		turnCounter += 1
+	#the cpu turn
+	else:
+		while(True):
+			cpuChoice = random.choice(possibleNumbers)
+			print("\nCpu choice: ", cpuChoice)
+			if (cpuChoice in possibleNumbers):
+				modifyArray(cpuChoice, 'O')
+				possibleNumbers.remove(cpuChoice)
+				turnCounter += 1
+				break
+
